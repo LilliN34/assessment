eaaa137f86af2ba602cde410ba431af924eb4e19
LilliN34 <chihiro.yamagishi2006@gmail.com>
2025-11-20 07:17:25 +0900

    デザイン改良
    デザイン改良

--- assessment.js
+++ assessment.js
@@ -15,14 +15,28 @@ assessmentButton.addEventListener(//イベント検知設定の追加
 
     //診断結果表示エリアの作成
     resultDivision.innerText = '';//divタグを空文字で上書き
-    const header = document.createElement('h3')//h3タグの作成
-    header.innerText = '診断結果';//タグの内側のテキストを設定
-    resultDivision.appendChild(header)//divタグの子要素として追加
+    const headerDivision = document.createElement('div')//divタグの作成
+    headerDivision.setAttribute('class', 'card-header text-bg-primary')//class設定
+    headerDivision.innerText = '診断結果';//タグの内側のテキストを設定
+
+
+
+   //bodyDivisionの作成
+  const bodyDivision =document.createElement('div');
+  bodyDivision.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p')//pタグの作成
+    paragraph.setAttribute('class','card-text');
     const result = assessment(userName)//診断結果を作成
     paragraph.innerText = result//pタグの内側のテキストを設定
-    resultDivision.appendChild(paragraph)//divタグの子要素としてpタグを追加
+    bodyDivision.appendChild(paragraph)
+
+    // resultDivisionにBootstrapのスタイルを適用
+
+    resultDivision.setAttribute('class', 'card');
+
+    resultDivision.appendChild(headerDivision)//divタグの子要素として追加
+    resultDivision.appendChild(bodyDivision)
 
     //X投稿ボタンの作成
     tweetDivision.innerText = '';//Tweetのdivタグも空にする
